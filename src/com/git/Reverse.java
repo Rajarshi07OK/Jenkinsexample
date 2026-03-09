ae65e04e6d4a5a1ead23351bea6aedc3b4158ee4
Rajarshi07OK <karmakar1raj@gmail.com>
2026-03-09 12:15:34 +0530

    Reversed pushed

---
+++ src/com/git/Reverse.java
@@ -0,0 +1,10 @@
+package com.git;
+
+public class Reverse {
+	public static void main(String[] args) {
+	String str="SaranMadar";
+	StringBuilder sb = new StringBuilder();
+	sb.append(str);
+	System.out.println(sb.reverse());
+	}
+}
